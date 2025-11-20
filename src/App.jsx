b5324ef8987f89fd7f29a7038edf4a79b0fb54f7
wugkqd <wugkqd@gmail.com>
2025-11-20 18:26:48 +0900

    ✨TanStack Query사용

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
 import { Routes, Route } from 'react-router-dom'
-import { useState, useEffect } from 'react'
+//import { useState, useEffect } from 'react'
+import { useQuery } from '@tanstack/react-query'
 import SongList from './pages/SongList.jsx'
 import SongDetail from './pages/SongDetail.jsx'
 import { getSongList } from './api/songApi'
 
 function App() {
-  const [songs, setSongs] = useState([])
 
-  useEffect(() => {
-    const fetchSongs = async () => {
-      try {
-        const data = await getSongList()
-        setSongs(data)
-      } catch (err) {
-        console.error("Failed to fetch songs:", err)
-      }
-    }
+  const { data: songs, isLoading, isError, error } = useQuery({
+    queryKey: ['songs'],
+    queryFn: getSongList
+  })
 
-    fetchSongs()
-  }, [])
+  if (isLoading) {
+    return <p className="text-center mt-10">Loading...</p>
+  }
+
+  if (isError) {
+    return <p className="text-center mt-10">오류 발생: {error.message}</p>
+  }
 
   return (
     <Routes>
