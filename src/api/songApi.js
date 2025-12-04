005f10e7929e7c9b7eae6bad3eace287ee5f7e38
wugkqd <wugkqd@gmail.com>
2025-12-04 17:20:51 +0900

    🔖backend 연결

--- src/api/songApi.js
+++ src/api/songApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api/songs',
+  baseURL: 'https://w25w11-mongodb-backend-f4z8.onrender.com/api/songs',
 })
 
 export const getSongList = async () => {
